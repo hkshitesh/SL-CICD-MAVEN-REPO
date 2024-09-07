46ab715bbbe266b4c2e708070cb0db0b57e26ca3
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-09-07 12:53:16 +0530

    mod() added

--- src/main/java/mypkg/MyCalculator.java
+++ src/main/java/mypkg/MyCalculator.java
@@ -17,12 +17,17 @@ public class MyCalculator {
 	{
 		return a/b;
 	}
+	int mod(int a, int b)
+	{
+		return a%b;
+	}
 	public static void main(String[] args) {
 
 		MyCalculator calc = new MyCalculator();
 		System.out.println("Sum is : "+calc.sum(20,10));
 		System.out.println("Diff is : "+calc.diff(20,10));	
 		System.out.println("Mul is : "+calc.mul(20,10));
-		System.out.println("Mul is : "+calc.div(20,10));
+		System.out.println("Div is : "+calc.div(20,10));
+		System.out.println("Mod is : "+calc.mod(20,10));
 	}
 }
